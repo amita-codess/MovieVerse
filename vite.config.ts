545f919b23d029d01f549a1ebd076b99e06359b5
Amita Dongardive <amitadongardive22@gmail.com>
2026-08-21 15:12:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/MovieVerse/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
